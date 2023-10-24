89868497247270edb6cf6b473e93591461f7990e
midrissi01 <m.oulhaj_idrissi@mundiapolis.ma>
2023-10-24 11:22:56 +0100

    adding url

--- express2.js
+++ express2.js
@@ -1,4 +1,4 @@
-//nUzZcP1S4A48RFjx
+
 
 const express = require("express");
 
@@ -6,8 +6,7 @@ const app = express();
 const productrouter = require("./routes/products.routes");
 const mongoose = require("mongoose");
 
-const URL =
-  "mongodb+srv://elmirmehdi96:nUzZcP1S4A48RFjx@cluster0.bkinbay.mongodb.net/products_db?retryWrites=true&w=majority";
+const URL ="mongodb+srv://moulhaj430:Usht7hazZeLRZHGQ@cluster0.usaa3js.mongodb.net/Product_DB?retryWrites=true&w=majority";
 
 mongoose
   .connect(URL)
